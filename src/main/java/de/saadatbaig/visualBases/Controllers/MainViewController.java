9a6952c2f9ce912c919ed06480bde2391434b105
mass1ve-err0r <me@saadat.dev>
2021-02-24 19:01:23 +0100

    update MainViewController + added input logic checks for the decimal point --> other chars are still possible, will fix later + added smarter textfield interaction logic, upon click active textfield is set through int "active" for later processing

--- src/main/java/de/saadatbaig/visualBases/Controllers/MainViewController.java
+++ src/main/java/de/saadatbaig/visualBases/Controllers/MainViewController.java
@@ -31,9 +31,12 @@ public class MainViewController {
     @FXML public CheckBox cbIntegerMode;
     @FXML public CheckBox cbRationalMode;
     HostServices _services;
+    int active = 0;
+
 
     public MainViewController() {
         setupListeners();
+        setupInputLogic();
     }
 
     public void setHostServices(HostServices hostServices) {
@@ -45,6 +48,7 @@ public class MainViewController {
             cbIntegerMode.setOnAction(evt -> {
                 if (cbIntegerMode.selectedProperty().get()) {
                     cbRationalMode.selectedProperty().set(false);
+                    cleanUpInput();
                 } else {
                     cbRationalMode.selectedProperty().set(true);
                 }
@@ -54,11 +58,72 @@ public class MainViewController {
                     cbIntegerMode.selectedProperty().set(false);
                 } else {
                     cbIntegerMode.selectedProperty().set(true);
+                    cleanUpInput();
                 }
             });
 
             cbIntegerMode.requestFocus();
         });
+    }
+
+    private void setupInputLogic() {
+        Platform.runLater(() -> {
+            tfDec.textProperty().addListener((observableValue, s1, s2) -> {
+
+                if (cbRationalMode.selectedProperty().get()) {
+                    if (s2.chars().filter(ch -> ch == '.').count() > 1) {
+                        tfDec.setText(s1);
+                    }
+                } else {
+                    if (s2.contains(".")) {
+                        tfDec.setText(s1);
+                    }
+                }
+            });
+
+        });
+    }
+
+    private void cleanUpInput() {
+        String fieldText = tfAccess(true, null);
+        if (fieldText.contains(".")) {
+            fieldText = fieldText.chars().filter(i -> ((char)i != '.')).collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+            tfAccess(false, fieldText);
+        }
+    }
+
+    private String tfAccess (boolean get, String text) {
+        switch (active) {
+            case 0:
+                if (get) {
+                    return tfDec.getText();
+                } else {
+                    tfDec.setText(text);
+                }
+                break;
+            case 1:
+                if (get) {
+                    return tfBin.getText();
+                } else {
+                    tfBin.setText(text);
+                }
+                break;
+            case 2:
+                if (get) {
+                    return tfHex.getText();
+                } else {
+                    tfHex.setText(text);
+                }
+                break;
+            case 3:
+                if (get) {
+                    return tfOct.getText();
+                } else {
+                    tfOct.setText(text);
+                }
+                break;
+        }
+        return null;
     }
 
 
